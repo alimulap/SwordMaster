6963990da38ff094c0cb0c913bac0a16c9a33a9b
alimulap <mohammad4limul@gmail.com>
2024-12-25 09:51:30 +0700

    player attack + animation

--- Assets/Scripts/TheSwordMaster.cs
+++ Assets/Scripts/TheSwordMaster.cs
@@ -1,5 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
+// using System.Collections;
+// using System.Collections.Generic;
 using UnityEngine;
 
 public class TheSwordMaster : CharacterController2D
@@ -8,12 +8,19 @@ public class TheSwordMaster : CharacterController2D
     Animator animator;
 
     bool shouldJump = false;
-    bool isJumping = false;
+
+    // bool isJumping = false;
+    bool isAttacking = false;
+    bool readyNextAtt = true;
+    bool finishedAnAtt = true;
+    bool comboTriggered = false;
     bool isDashing = false;
     bool isRolling = false;
 
     MoveDir moveDir = MoveDir.None;
     FaceDir facing = FaceDir.Right;
+    Attack currAtt = Attack.None;
+    Attack nextAtt = Attack.None;
 
     public override void Start()
     {
@@ -25,7 +32,28 @@ public class TheSwordMaster : CharacterController2D
 
     public override void Update()
     {
-        if (this.isOnGround && (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.Space)))
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            if (this.currAtt.Equals(Attack.None))
+            {
+                this.currAtt = Attack.Slash1;
+                this.nextAtt = Attack.Slash1;
+                this.animator.SetTrigger("slash1");
+                this.readyNextAtt = false;
+            }
+            else if (this.readyNextAtt)
+            {
+                this.comboTriggered = true;
+                this.readyNextAtt = false;
+                this.animator.SetTrigger("combo");
+            }
+        }
+
+        if (
+            this.isOnGround
+            && this.currAtt == Attack.None
+            && (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.Space))
+        )
         {
             this.shouldJump = true;
         }
@@ -33,13 +61,13 @@ public class TheSwordMaster : CharacterController2D
         bool rightDown = Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow);
         bool leftDown = Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow);
 
-        switch (rightDown, leftDown)
+        switch (rightDown, leftDown, this.currAtt == Attack.None)
         {
-            case (true, false):
+            case (true, false, true):
                 this.moveDir = MoveDir.Right;
                 this.facing = FaceDir.Right;
                 break;
-            case (false, true):
+            case (false, true, true):
                 this.moveDir = MoveDir.Left;
                 this.facing = FaceDir.Left;
                 break;
@@ -62,11 +90,17 @@ public class TheSwordMaster : CharacterController2D
     public override void FixedUpdate()
     {
         this.Gravitate();
+        this.AttackHandler();
         this.JumpHandler();
         this.Accelerate();
         this.Move();
         this.AnimatorController();
-        Debug.Log("IsOnGround: " + this.isOnGround);
+        // Debug.Log("Vel: " + this.velocity);
+    }
+
+    void AttackHandler()
+    {
+        if (this.finishedAnAtt) { }
     }
 
     void JumpHandler()
@@ -111,6 +145,51 @@ public class TheSwordMaster : CharacterController2D
         this.velocity += this.gravity * Time.fixedDeltaTime;
     }
 
+    void ReadyNextAttack()
+    {
+        this.readyNextAtt = true;
+    }
+
+    void AttackFinished()
+    {
+        if (this.comboTriggered)
+        {
+            this.comboTriggered = false;
+            switch (this.nextAtt)
+            {
+                case Attack.Slash1:
+                    this.currAtt = Attack.Slash1;
+                    this.nextAtt = Attack.Slash2;
+                    break;
+                case Attack.Slash2:
+                    this.currAtt = Attack.Slash2;
+                    this.nextAtt = Attack.SpinAttack;
+                    break;
+                case Attack.SpinAttack:
+                    this.currAtt = Attack.SpinAttack;
+                    this.nextAtt = Attack.Slam;
+                    break;
+                case Attack.Slam:
+                    this.currAtt = Attack.Slam;
+                    this.nextAtt = Attack.SpinAttack;
+                    break;
+                case Attack.RollAttack:
+                    this.currAtt = Attack.RollAttack;
+                    this.nextAtt = Attack.None;
+                    break;
+                case Attack.None:
+                    this.currAtt = Attack.None;
+                    break;
+            }
+        }
+        else
+        {
+            this.currAtt = Attack.None;
+            this.nextAtt = Attack.None;
+            this.animator.SetTrigger("exit_combo");
+        }
+    }
+
     void DashEnds()
     {
         this.isDashing = false;
@@ -133,4 +212,14 @@ enum FaceDir
 {
     Right,
     Left,
+}
+
+enum Attack
+{
+    Slash1,
+    Slash2,
+    SpinAttack,
+    Slam,
+    RollAttack,
+    None,
 }
